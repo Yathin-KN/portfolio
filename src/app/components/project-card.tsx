0a0649dbfc88d237cb6f7e4dfe815f6ccc834aeb
Yathin-KN <yathin2184@gmail.com>
2024-01-26 18:19:56 +0530

    v1

---
+++ src/app/components/project-card.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Image from "next/image";
+import Arrow from "./../assets/arrow.svg";
+import Project from "./../assets/project1.svg";
+import clsx from "clsx";
+import useMousePosition from "../utils/ueMousePosition";
+import { motion } from "framer-motion";
+import Mask from "./../assets/mask.svg"
+import { useState } from "react";
+const ProjectCard = () => {
+  const { x, y } = useMousePosition();
+  const [isHovered, setIsHovered] = useState(false);
+
+
+  return (
+    <div className="flex justify-between py-2 pt-10 px-1 relative space-x-10">
+      <motion.div className="w-fit"  >
+        <div className="flex space-x-[8rem] animate-custom cursor-pointer">
+          <motion.div
+            onHoverStart={() => setIsHovered(true)}
+        onHoverEnd={() => setIsHovered(false)}
+            className={clsx(
+              "text-7xl font-semibold mix-blend-difference text-font-primary z-20 text-opacity-90 font-grotesk trans1"
+            )}
+            style={{
+              color:"#ebe7dd"
+            }}
+          >
+            FANDB SYS
+          </motion.div>
+          <div className={clsx("py-2 trans2 mix-blend-difference z-50")}>
+            <Image src={Arrow} alt="" height={40} width={40} />
+          </div>
+        </div>
+        <div className="px-1 flex justify-between pt-8 ">
+          <div>Fullstack Engineer</div>
+          <div className="text-right ">Apr'22 - Oct'22</div>
+        </div>
+        <div className="text-2xl mt-4 font-grotesk-medium">
+          Table management <br /> System <br/>
+          {x} {y}
+        </div>
+      </motion.div>
+      <div className="flex-0 w-fit rounded-md">
+        <Image src={Project} alt="" height={220} width={220} />
+      </div>
+
+      <motion.div className="fixed z-10 w-fit h-fit" style={{
+         transformOrigin: "center center",
+      }} animate={{
+        x:x-130,
+        y:y-160,
+        scale: isHovered ? 2.5 : 1,
+      }}>
+        <Image src={Mask} alt="" height={50} width={50}  className="z-10" />
+      </motion.div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,23 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors:{
+      "primary-bg":"#e1dfdd",
+      "font-primary":"#282828",
+      "font-secondary":"000000",
+      "grotesq-color":"1c1c1c"
+    },
+    fontFamily:{
+      "mono":['Space Mono','monospace'],
+      // "grotesk":['Familjen Grotesk', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
+    }
   },
   plugins: [],
 };
